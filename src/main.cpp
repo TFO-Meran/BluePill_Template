967f97aa94022fc37b69f9c02101fb6286b932e0
cg <chris@gaiser.it>
2025-09-27 23:20:39 +0200

    Test..

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,31 @@
-/*
- * ---- Project Template ----
- * 
- * by CGProjects
- *  
- *
- */
-
 // ======================================================================================================
 // ********************************* PROJECT TEMPLATE: MAIN *********************************************
 // ======================================================================================================
 
 #include <Arduino.h>
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN PC13
-#endif
+#define LED_PIN PC13
+#define CYCLETIME 500
 
 void setup()
 {
-  // Serial.begin(115200);
-  Serial.begin(115200);
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("\nHello BluePill!!!\n\n");
+    pinMode(LED_PIN, OUTPUT);
 }
+
+uint8_t counter = 0;
+uint32_t lastcall = 0;
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  delay(100);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  // wait for a second
-  delay(900);
-  Serial.println("STM32 READY!!!");
+    if (millis() - lastcall > CYCLETIME)
+    {
+        lastcall = millis();
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        Serial.println(counter++);
+    }
 }
 // ======================================================================================================
 // ***************************************** END OF FILE ************************************************
